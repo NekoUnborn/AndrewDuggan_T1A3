8ad6c81891188ed771b86845c6f386d68b52126d
neko <neko@LAPTOP-V5T2GBSE.localdomain>
2021-04-02 14:23:42 +1000

    Added validate_input module

--- main.rb
+++ main.rb
@@ -1,2 +1,5 @@
-require colorize
+require "colorize"
+require_relative "./modules/log_in"
+require_relative "./modules/validate_input"
 # require_relative './log_in.rb'
+log_in_module
---
+++ modules/log_in.rb
@@ -0,0 +1,79 @@
+def log_in_module
+  require 'csv'
+  require_relative './validate_input'
+
+  quit = false
+  users = CSV.open("./saved_data/users.csv", "a+")
+  user = {}
+
+  def get_login_details
+    username = validate_input("username", "your username")
+    password = validate_input("password", "a password")
+    [username, password]
+  end
+
+  def find_user(username)
+    CSV.open("./saved_data/users.csv", "a+") do |csv|
+      csv.each do |line|
+        return line if line[0] == username
+        p line
+
+        return !line if line[0] != username
+      end
+    end
+  end
+
+  def append_csv(username, password)
+    CSV.open("./saved_data/users.csv", "a") do |csv|
+      csv << [username, password]
+    end
+  end
+
+  until (user != {}) || quit
+    system "clear"
+    puts "what would you like to do?"
+    puts "options: [login, signup, quit]"
+    input = gets.chomp
+
+    case input
+
+      # login
+    when "login"
+      username, password = get_login_details
+      line = find_user(username)
+      if line
+        if line[1] == password
+          user[:username] = username
+          user[:password] = password
+          puts "You are logged in"
+        else
+          puts "Incorrect password"
+        end
+      else
+        puts "Username does not exist, sign up"
+      end
+
+      # signup
+    when "signup"
+      username, password = get_login_details
+      username_taken = find_user(username)
+      if username_taken
+        puts "Username is taken"
+      else
+        append_csv(username, password)
+        user[:username] = username
+        user[:password] = password
+        puts "New login created"
+      end
+
+      # quit
+    when "quit"
+      quit = true
+
+    else
+      puts "Incorrect option"
+    end
+
+    sleep 3
+  end
+end
